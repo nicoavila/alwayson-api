e49f2e0b0615008cc9e6b553d820f664c6cd9a81
Nicolas Avila <nicolasavila.a@gmail.com>
2018-04-18 21:42:12 -0300

    habilita cors y commit de bin

--- app.js
+++ app.js
@@ -73,6 +73,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Habilita CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 //Uso de rutas
 app.use('/', indexRouter);
 app.use('/usuarios', usuariosRouter);
